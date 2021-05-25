db77d67393750b11d8f5eb0fb470eebaff889392
Shanen Cross <shanencross@yahoo.com>
2021-05-25 08:19:38 -0700

    Add Index route of stylist controller

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController: Controller
+  {
+    HairSalonContext _db;
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+  }
+}
