a2a78cfeb2ace7e6ef0a58d793a398d1bf6cfa10
Shanen Cross <shanencross@yahoo.com>
2021-05-25 08:21:18 -0700

    Add Ids to client and stylist models

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,6 +2,7 @@ namespace HairSalon.Models
 {
   public class Client
   {
+    public int ClientId { get; set; }
     public string Name { get; set; }
     public string HairColor { get; set; }
   }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,6 +4,7 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
+    public int StylistId { get; set; }
     public string Name { get; set; }
     public string Specialty { get; set; }
     public virtual ICollection<Client> Clients { get; set; }
