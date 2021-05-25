7714d548706a7a4b562ff60548b9170eec599d4d
Shanen Cross <shanencross@yahoo.com>
2021-05-25 10:12:17 -0700

    Add stylist id to client

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,6 +3,7 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId { get; set; }
+    public int StylistId { get; set; }
     public string Name { get; set; }
     public string HairColor { get; set; }
   }
