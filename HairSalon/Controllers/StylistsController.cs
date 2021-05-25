fdbe1e30c1f51559c62ab74190627173a2f921d3
Shanen Cross <shanencross@yahoo.com>
2021-05-25 08:52:07 -0700

    Add post Create action

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -23,5 +23,13 @@ namespace HairSalon.Controllers
     {
       return View();
     }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
