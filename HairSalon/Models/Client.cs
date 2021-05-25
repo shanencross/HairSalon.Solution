d82b692c462473e4f57e526e6eea754bd6e0a43e
Shanen Cross <shanencross@yahoo.com>
2021-05-25 07:37:16 -0700

    Fill out Client and Stylist models

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,6 +2,7 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    
+    public string Name { get; set; }
+    public string HairColor { get; set; }
   }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+
 namespace HairSalon.Models
 {
   public class Stylist
   {
-    
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+
+    public Stylist()
+    {
+      Clients = new HashSet<Client>();
+    }
   }
 }
