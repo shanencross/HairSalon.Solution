f221effd41ed4aa0989a7990b6fb20213f3a54e6
Shanen Cross <shanencross@yahoo.com>
2021-05-25 07:32:48 -0700

    Write Hair Salon Context and skeleton for Client and Stylist

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,6 +4,14 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
+    public virtual DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Client> Clients { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+
+    public HairSalonContext(DbContextOptions options): base(options) { }
   }
 }
