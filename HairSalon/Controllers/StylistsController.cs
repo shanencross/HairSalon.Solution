a9be39cb2de3e7169caeda55c9665210eca89529
Shanen Cross <shanencross@yahoo.com>
2021-05-25 08:30:33 -0700

    At Create view and get route

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -18,5 +18,10 @@ namespace HairSalon.Controllers
       List<Stylist> model = _db.Stylists.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
   }
 }
